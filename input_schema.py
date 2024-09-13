802a6841ba1974c3956d4742ddc3b951ef53e000
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-14 00:47:19 +0530

    Add files via upload

---
+++ input_schema.py
@@ -0,0 +1,15 @@
+INPUT_SCHEMA = {
+    "articles": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [-1],
+        'example': [
+            "Diagnosis and Management of Central Diabetes Insipidus in Adults",
+            "Central diabetes insipidus (CDI) is a clinical syndrome which results from loss or impaired function of vasopressinergic neurons in the hypothalamus/posterior pituitary, resulting in impaired synthesis and/or secretion of arginine vasopressin (AVP). [...]",
+            "Adipsic diabetes insipidus",
+            "Adipsic diabetes insipidus (ADI) is a rare but devastating disorder of water balance with significant associated morbidity and mortality. Most patients develop the disease as a result of hypothalamic destruction from a variety of underlying etiologies. [...]",
+            "Nephrogenic diabetes insipidus: a comprehensive overview",
+            "Nephrogenic diabetes insipidus (NDI) is characterized by the inability to concentrate urine that results in polyuria and polydipsia, despite having normal or elevated plasma concentrations of arginine vasopressin (AVP). [...]"
+      ]
+    }
+}
